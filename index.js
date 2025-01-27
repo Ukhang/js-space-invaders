9d67458f4ac4a84e63d7a391f45807494da3a3fd
Ukhang <ukhangmarma459@gmail.com>
2025-01-27 11:44:25 +0600

    create and move grids of invader

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ class Projectile {
 }
 
 class Invader {
-  constructor() {
+  constructor({ position }) {
     this.velocity = {
       x: 0,
       y: 0,
@@ -100,8 +100,8 @@ class Invader {
       this.width = image.width * scale;
       this.height = image.height * scale;
       this.position = {
-        x: canvas.width / 2 - this.width / 2,
-        y: canvas.height / 2
+        x: position.x,
+        y: position.y,
       };
     };
   }
@@ -117,21 +117,66 @@ class Invader {
       this.width,
       this.height
     );
+  }
 
+  update({ velocity }) {
+    if (this.image) {
+      this.draw();
+      this.position.x += velocity.x;
+      this.position.y += velocity.y;
+    }
+  }
+}
+
+class Grid {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+
+    this.velocity = {
+      x: 3,
+      y: 0,
+    };
+
+    this.invaders = [];
+
+    const columns = Math.floor(Math.random() * 10 + 5);
+    const rows = Math.floor(Math.random() * 5 + 2);
+
+    this.width = columns * 30;
+
+    for (let x = 0; x < columns; x++) {
+      for (let y = 0; y < rows; y++) {
+        this.invaders.push(
+          new Invader({
+            position: {
+              x: x * 30,
+              y: y * 30,
+            },
+          })
+        )
+      }
+    }
   }
 
   update() {
-    if (this.image) {
-      this.draw();
-      this.position.x += this.velocity.x;
-      this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    this.velocity.y = 0;
+
+    if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x;
+      this.velocity.y = 30;
     }
   }
 }
 
 const player = new Player();
 const projectiles = [];
-const invader = new Invader();
+const grids = [new Grid()];
 
 const keys = {
   a: {
@@ -149,17 +194,22 @@ function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
-  invader.update();
   player.update();
   projectiles.forEach((projectile, index) => {
+    if (projectile.position.y + projectile.radius <= 0) {
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }, 0);
+    } else {
+      projectile.update();
+    }
+  });
 
-		if (projectile.position.y + projectile.radius <= 0) {
-			setTimeout(() => {
-				projectiles.splice(index, 1);
-			}, 0);
-		} else {
-			projectile.update();
-		}
+  grids.forEach((grid) => {
+    grid.update();
+    grid.invaders.forEach((invader) => {
+      invader.update({ velocity: grid.velocity });
+    });
   });
 
   if (keys.a.pressed && player.position.x >= 0) {
@@ -200,7 +250,7 @@ window.addEventListener("keydown", ({ key }) => {
           },
         })
       );
-      
+
       break;
   }
 });
