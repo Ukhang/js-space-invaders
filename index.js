a3c28a303fe2af93bc0085cdd86253d6f8f1c708
Ukhang <ukhangmarma459@gmail.com>
2025-01-27 12:35:22 +0600

    spawn grids at intervals

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ class Grid {
 
 const player = new Player();
 const projectiles = [];
-const grids = [new Grid()];
+const grids = [];
 
 const keys = {
   a: {
@@ -189,6 +189,9 @@ const keys = {
     pressed: false,
   },
 };
+
+let frames = 0;
+let randomInterval =  Math.floor((Math.random() * 500) + 500);
 
 function animate() {
   requestAnimationFrame(animate);
@@ -225,6 +228,14 @@ function animate() {
     player.velocity.x = 0;
     player.rotation = 0;
   }
+
+  if (frames % randomInterval === 0) {
+    grids.push(new Grid());
+    randomInterval =  Math.floor((Math.random() * 500) + 500);
+    frames = 0;
+  }
+
+  frames++;
 }
 
 animate();
